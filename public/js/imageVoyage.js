baaa61593ddead795842252116330ab872adf006
Jean Daniel Boccara <jdboccara@gmail.com>
2019-12-04 09:39:14 +0100

    commit avant ajouts entite Etape et localisation

--- public/js/imageVoyage.js
+++ public/js/imageVoyage.js
@@ -1,19 +1,14 @@
 $('#add-image').click(function () {
 
-    // je récupère le numéro des futurs champs que je vais créer
     const index = +$('#widgets-counter').val();
 
-    // je récupère le proto des entrées
     const tmpl = $('#voyage_images').data('prototype').replace(/__name__/g, index);
     console.log(tmpl);
 
-    //j'injecte ce code dans la div
     $('#voyage_images').append(tmpl);
-
 
     $('#widgets-counter').val(index +1);
 
-    //je gère le bouton supprimer
     handleDeleteButtons();
 });
 
